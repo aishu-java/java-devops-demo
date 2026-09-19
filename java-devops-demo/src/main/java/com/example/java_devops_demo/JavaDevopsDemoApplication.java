40750c84d59cd2ac3bb1d9acec6ef5eb4f2f1232
Aishwarya <aishwarya89sharma@gmail.com>
2026-09-19 23:42:31 +0530

    Initial commit

---
+++ java-devops-demo/src/main/java/com/example/java_devops_demo/JavaDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_devops_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevopsDemoApplication.class, args);
+	}
+
+}
